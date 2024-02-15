a0b056f88b9caae92836312ab9042511ce3ee590
Bayu Rayhan <bayurayhan36@gmail.com>
2024-02-15 15:03:41 +0700

    Fixing webhook registration system using only one file

--- register_webhook.py
+++ register_webhook.py
@@ -1,42 +1,49 @@
-from pyngrok import ngrok
-import os
 import requests
+import os
+import sys
 from dotenv import load_dotenv
+from pyngrok import ngrok
 
 load_dotenv()
 
-PORT = os.environ.get("SERVER_PORT")
 API_KEY = os.environ.get("TELEGRAM_API_KEY")
-http_tunnel = ngrok.connect(f"http://localhost:{PORT}")
+PORT = os.environ.get("SERVER_PORT")
+SERVER_DOMAIN = os.environ.get("SERVER_DOMAIN")
 
-print(http_tunnel.public_url)
+def register_webhook(is_server):
+    if is_server:
+        url = f"https://api.telegram.org/bot{API_KEY}/setWebhook?url=https://{SERVER_DOMAIN}/api/webhook"
+    else:
+        http_tunnel = ngrok.connect(f"http://localhost:{PORT}")
+        print(http_tunnel.public_url)
+        url = f"https://api.telegram.org/bot{API_KEY}/setWebhook?url={http_tunnel.public_url}/api/webhook"
 
-url_webhook = f"https://api.telegram.org/bot{API_KEY}/setWebhook?url={http_tunnel.public_url}/api/webhook"
-res = requests.get(url_webhook)
-# Check the status code
-if res.status_code == 200:
-    # Print the response content
-    print(res.text)
-else:
-    raise RuntimeError(res.status_code)
+    res = requests.get(url)
 
-webhook_info = f"https://api.telegram.org/bot{API_KEY}/getWebhookInfo"
+    if res.status_code == 200:
+        print(res.text)
+    else:
+        raise RuntimeError(res.status_code)
 
-res = requests.get(webhook_info)
-# Check the status code
-if res.status_code == 200:
-    # Print the response content
-    print(res.text)
-else:
-    raise RuntimeError(res.status_code)
+    webhook_info = f"https://api.telegram.org/bot{API_KEY}/getWebhookInfo"
+    res = requests.get(webhook_info)
 
+    if res.status_code == 200:
+        print(res.text)
+    else:
+        raise RuntimeError(res.status_code)
 
-ngrok_process = ngrok.get_ngrok_process()
+    if not is_server:
+        ngrok_process = ngrok.get_ngrok_process()
+        try:
+            ngrok_process.proc.wait()
+        except KeyboardInterrupt:
+            print("Shutting down server.")
+            ngrok.kill()
 
-try:
-    # Block until CTRL-C or some other terminating event
-    ngrok_process.proc.wait()
-except KeyboardInterrupt:
-    print(" Shutting down server.")
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or sys.argv[1] not in ["server", "local"]:
+        print("Usage: python combined_script.py [server|local]")
+        sys.exit(1)
 
-    ngrok.kill()
+    register_webhook(sys.argv[1] == "server")
