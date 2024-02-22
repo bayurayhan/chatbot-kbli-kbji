f49b00c0a9affafd5969fa76bad32a665531bb71
Bayu Rayhan <bayurayhan36@gmail.com>
2024-02-22 21:58:22 +0700

    Applying singleton

--- chatbot/Application.py
+++ chatbot/Application.py
@@ -21,6 +21,13 @@ logger = logging.getLogger("app")
 
 
 class Application:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self, server: fastapi.FastAPI):
         self.server = server
         self.config_file = get_path("config.yaml")
