f9d777f88bff065bbe6d76d1757b91e4aa707a89
Bayu Rayhan <bayurayhan36@gmail.com>
2024-02-22 22:07:28 +0700

    Setting the workers variable

--- main.py
+++ main.py
@@ -23,12 +23,6 @@ PORT = int(os.environ.get("SERVER_PORT", 8000))
 # Run the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    SSL_CERT = os.environ.get("SSL_CERT")
-    SSL_KEY = os.environ.get("SSL_KEY")
-    DEBUG = os.environ.get("DEBUG")
-    if SSL_CERT and SSL_KEY:
-        logger.info("Starting application in HTTPS mode...")
-        uvicorn.run("main:server", host=HOST, port=PORT, reload=(DEBUG == "true"), ssl_certfile=SSL_CERT, ssl_keyfile=SSL_KEY, reload_excludes="*.log*")
-    else:
-        logger.info("Starting application in HTTP mode...")
-        uvicorn.run(server, host=HOST, port=PORT)
+    logger.info("Starting application in HTTP mode...")
+    WORKERS = os.environ.get("WORKERS", None)
+    uvicorn.run("main:server", host=HOST, port=PORT, workers=WORKERS)
