303cf9c0bb5cd242a421933110067e80577fd2dd
Bayu Rayhan <bayurayhan36@gmail.com>
2024-03-06 19:25:24 +0700

    Adding TQDM while downloading resource

--- download_embedding.py
+++ download_embedding.py
@@ -1,18 +1,23 @@
 import os
 import requests
 import zipfile
+from tqdm import tqdm
 from chatbot.utils import *
 
 def download_and_extract(url, save_path):
     # Download the file
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as f:
-            f.write(response.content)
-        print("Download complete.")
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
-        return
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 KB
+    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+
+    with open(save_path, 'wb') as f:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            f.write(data)
+
+    progress_bar.close()
+    print("Download complete.")
 
     # Extract the file
     with zipfile.ZipFile(save_path, 'r') as zip_ref:
